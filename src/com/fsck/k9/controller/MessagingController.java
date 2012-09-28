a375b8d8d0d111cb19c063b01e4e087a05b109c6
Jacek Sowiński <mruwek@gmail.com>
2012-09-28 21:49:40 +0200

    Switch from  NotificationBuilder to NotificationCompat.Builder
    This means switch from our own compatibility layer to android support's
    one.

--- src/com/fsck/k9/controller/MessagingController.java
+++ src/com/fsck/k9/controller/MessagingController.java
@@ -29,6 +29,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.PowerManager;
 import android.os.Process;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -43,7 +44,6 @@ import com.fsck.k9.R;
 import com.fsck.k9.SearchSpecification;
 import com.fsck.k9.activity.FolderList;
 import com.fsck.k9.activity.MessageList;
-import com.fsck.k9.helper.NotificationBuilder;
 import com.fsck.k9.helper.Utility;
 import com.fsck.k9.helper.power.TracingPowerManager;
 import com.fsck.k9.helper.power.TracingPowerManager.TracingWakeLock;
@@ -3238,7 +3238,7 @@ public class MessagingController implements Runnable {
         NotificationManager notifMgr =
             (NotificationManager) mApplication.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationBuilder builder = NotificationBuilder.createInstance(mApplication);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mApplication);
         builder.setSmallIcon(R.drawable.ic_menu_refresh);
         builder.setWhen(System.currentTimeMillis());
         builder.setOngoing(true);
@@ -3285,7 +3285,7 @@ public class MessagingController implements Runnable {
         NotificationManager notifMgr =
                 (NotificationManager) mApplication.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationBuilder builder = NotificationBuilder.createInstance(mApplication);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mApplication);
         builder.setSmallIcon(R.drawable.stat_notify_email_generic);
         builder.setWhen(System.currentTimeMillis());
         builder.setAutoCancel(true);
@@ -3320,7 +3320,7 @@ public class MessagingController implements Runnable {
         final NotificationManager notifMgr =
                 (NotificationManager) mApplication.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationBuilder builder = NotificationBuilder.createInstance(mApplication);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mApplication);
         builder.setSmallIcon(R.drawable.ic_menu_refresh);
         builder.setWhen(System.currentTimeMillis());
         builder.setOngoing(true);
@@ -4419,7 +4419,7 @@ public class MessagingController implements Runnable {
         NotificationManager notifMgr =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationBuilder builder = NotificationBuilder.createInstance(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
         builder.setSmallIcon(R.drawable.stat_notify_email_generic);
         builder.setWhen(System.currentTimeMillis());
         builder.setTicker(messageNotice);
@@ -4468,7 +4468,7 @@ public class MessagingController implements Runnable {
      * Configure the notification sound and LED
      *
      * @param builder
-     *         {@link NotificationBuilder} instance used to configure the notification.
+     *         {@link NotificationCompat.Builder} instance used to configure the notification.
      *         Never {@code null}.
      * @param ringtone
      *          String name of ringtone. {@code null}, if no ringtone should be played.
@@ -4482,7 +4482,7 @@ public class MessagingController implements Runnable {
      * @param ringAndVibrate
      *          {@code true}, if ringtone/vibration are allowed. {@code false}, otherwise.
      */
-    private void configureNotification(NotificationBuilder builder, String ringtone,
+    private void configureNotification(NotificationCompat.Builder builder, String ringtone,
             long[] vibrationPattern, Integer ledColor, int ledSpeed, boolean ringAndVibrate) {
 
         // if it's quiet time, then we shouldn't be ringing, buzzing or flashing
